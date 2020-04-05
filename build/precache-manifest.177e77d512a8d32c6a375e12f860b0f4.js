b8b4cdaa5d59bc42d4636b58286255d857bb8e3b
kweku76 <daffran@gmail.com>
2020-04-05 11:03:05 +1000

    update

---
+++ build/precache-manifest.177e77d512a8d32c6a375e12f860b0f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea10be384199afcc787a07e740ed25ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aae19f108936ac4cbc2",
+    "url": "/static/css/main.4c06968d.chunk.css"
+  },
+  {
+    "revision": "df22621fe4434a07ba4d",
+    "url": "/static/js/2.0df173f2.chunk.js"
+  },
+  {
+    "revision": "5aae19f108936ac4cbc2",
+    "url": "/static/js/main.919eb632.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
